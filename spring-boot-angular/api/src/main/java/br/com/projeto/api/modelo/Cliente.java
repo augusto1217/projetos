ae79e47187c6fb0650a7f2bb5370dd7771be23fc
Augusto <augusto1217@gmail.com>
2024-03-06 21:02:14 -0300

    Aprendendo spring-boot com angular

---
+++ spring-boot-angular/api/src/main/java/br/com/projeto/api/modelo/Cliente.java
@@ -0,0 +1,57 @@
+package br.com.projeto.api.modelo;
+
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+
+@Entity
+@Table(name = "clientes")
+public class Cliente {
+    
+    private long codigo;
+    private String nome;
+    private int idade;
+    private String cidade;
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(long codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+     
+}
