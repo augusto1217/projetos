2d32404287aeadea7c68a83671aed7e9eb4c3851
Augusto Mariano <augusto1217@gmail.com>
2024-05-27 22:32:51 -0300

    Evolução para criar o perfil de dev

--- udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/config/TestConfig.java
+++ udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/config/TestConfig.java
@@ -1,6 +1,7 @@
 package com.aug.helpdesk.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,15 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.aug.helpdesk.services.DBService;
 
 @Configuration
-@Profile("test")
+@Profile("dev")
 public class TestConfig {
 
     @Autowired
     private DBService dbService;
 
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String value;
+
     @Bean
     public Boolean instaciaDb() {
-        return this.dbService.instaciaDB();
+        if(value.equals("create")) {
+            this.dbService.instaciaDB();
+        }
+        return true;
     }
 
 }
