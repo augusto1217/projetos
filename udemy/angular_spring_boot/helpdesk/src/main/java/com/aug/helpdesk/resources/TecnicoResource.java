c5dff9913386cd83f0333bea4311d8ed4091e69a
Augusto Mariano <augusto1217@gmail.com>
2024-05-29 21:56:17 -0300

    Implementando padrão DTO

--- udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/resources/TecnicoResource.java
+++ udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/resources/TecnicoResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.aug.helpdesk.domain.Tecnico;
+import com.aug.helpdesk.domain.dtos.TecnicoDTO;
 import com.aug.helpdesk.services.TecnicoService;
 
 
@@ -19,9 +20,9 @@ public class TecnicoResource {
     private TecnicoService tecnicoService;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+    public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
         Tecnico tecnico = tecnicoService.findById(id);
-        return ResponseEntity.ok().body(tecnico);
+        return ResponseEntity.ok().body(new TecnicoDTO(tecnico));
     }
 
 }
