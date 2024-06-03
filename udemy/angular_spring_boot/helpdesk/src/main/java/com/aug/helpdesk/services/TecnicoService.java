b6b58fa0ca0df779dd1b3ccdee8e21fb52a177f4
Augusto Mariano <augusto1217@gmail.com>
2024-06-03 20:42:40 -0300

    Implementando pesquisa de todos os técnicos

--- udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/services/TecnicoService.java
+++ udemy/angular_spring_boot/helpdesk/src/main/java/com/aug/helpdesk/services/TecnicoService.java
@@ -1,5 +1,6 @@
 package com.aug.helpdesk.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,5 +19,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id) {
         Optional<Tecnico> tecnico = repository.findById(id);
         return tecnico.orElseThrow(() -> new ObjectNotFoundException("Id: " + id + " não encontrado!!!"));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
